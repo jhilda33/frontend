c7685d22726a6d19a353e1c9fbbb7877ef4bf9bb
Jhilda Achacollo <jhildaaliz@gmail.com>
2025-10-27 23:13:32 -0400

    feat(web): crear servicio empleados y consumir API

---
+++ src/app/services/empleado.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Empleado {
+  id?: number;
+  nombre: string;
+  apellido: string;
+  correo: string;
+  salario: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpleadoService {
+  private apiUrl = 'http://localhost:8000/api/empleados';
+
+  constructor(private http: HttpClient) { }
+
+  getEmpleados(): Observable<Empleado[]> {
+    return this.http.get<Empleado[]>(this.apiUrl);
+  }
+
+  createEmpleado(empleado: Empleado): Observable<Empleado> {
+    return this.http.post<Empleado>(this.apiUrl, empleado);
+  }
+}
